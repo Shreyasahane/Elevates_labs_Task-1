ac12e232cbb49a491dd78155203c54aece0c6718
Shreyasahane <sahaneshreya2003@gmail.com>
2026-01-15 20:07:00 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+# taking name from user
+name = input("Enter your name: ") 
+
+#taking Internship name that have been selected
+role =input("Enter Internship Role:")
+
+#display Todays date
+day = 15
+month ='January'
+year =2026
+
+# for displaying the user details as name, internship name and todays date
+print("Name: ",name)
+print("Internship Name: ",role)
+print("Todays Date: ",day,month,year)
